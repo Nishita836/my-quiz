339faec8d294230f1735890e5dd724190557a26e
Nishita836 <72186274+Nishita836@users.noreply.github.com>
2021-05-19 16:45:24 +0530

    Done1

--- Question.js
+++ Question.js
@@ -20,11 +20,11 @@ class Question{
         this.title.html("MyQuiz Game");
         this.title.position(350,0);
 
-        this.quetion.html("Question:- What starts and ends with the letter 'E', but has only one letter");
-        this.question.postion(150, 80);
+        this.question.html("Question:- What starts and ends with the letter 'E', but has onlt one letter");
+        this.question.position(150, 80);
         this.option1.html("1: Everyone");
         this.option1.position(150, 100);
-        this.option1.html("2: Evelope");
+        this.option2.html("2: Evelope");
         this.option2.position(150, 120);
 
         this.input1.position(150, 230);
